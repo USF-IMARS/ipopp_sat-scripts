4426da6536cbec90cc74b10c54703d0fa71e42ad
Tylar Murray <murray.tylar@gmail.com>
2017-11-20 21:04:39 +0000

    + docstring

--- sat-scripts/DayNight.sh
+++ sat-scripts/DayNight.sh
@@ -1,9 +1,30 @@
 #/bin/bash
+#
+# writes "DAY" or "NIGHT" to DayNightCheck.txt based on given geo_file.
+#
+# Parameters:
+# ------------------
+# geo_file : mxd03 file path like M{Y,O}D03.17234044500.hdf
+#    the file we want to check
+# Environment Variables:
+# MODIS_DB_HOME="/home/ipopp/drl/SPA/seadas7.3/algorithm"
+#
+# <Ncs_set name="geo_file" value="{drl.{sat}.modis.mxd03}"/>
+# <Ncs_set name="data_file" value="{drl.{sat}.modis.mxd021km}"/>
+# <Ncs_set name="output_file" value="{imars.{sat}.modis.oc}"/>
+#
+# Ported from [IMaRS Seadas SPA](https://github.com/USF-IMARS/seadas_spa)
+#     * SPA=seadas
+#     * station=modis_oc station [cfgfile](https://github.com/USF-IMARS/seadas_spa/blob/port/station/modis_oc/station.cfgfile)
+#     * algorithm=DayNight
+#         * [generic.xml](https://github.com/USF-IMARS/seadas_spa/blob/port/wrapper/oc/generic.xml)
+#         * [installation.xml](https://github.com/USF-IMARS/seadas_spa/blob/port/wrapper/oc/installation.xml)
+
+
 FIL1KM=$1
 file_type=`file ${FIL1KM} | grep 'Hierarchical Data Format (version 4) data'`
 
 echo ft: $file_type
-
 
 if [ -n "$file_type" ]
 then
