6e7bd4a543169d6ffbcf698e2d9e2b20dac31adf
Tylar Murray <murray.tylar@gmail.com>
2017-11-22 20:05:59 +0000

    make DayNightCheck fail when it should

--- sat-scripts/DayNight.sh
+++ sat-scripts/DayNight.sh
@@ -19,17 +19,19 @@
 #     * algorithm=DayNight
 #         * [generic.xml](https://github.com/USF-IMARS/seadas_spa/blob/port/wrapper/oc/generic.xml)
 #         * [installation.xml](https://github.com/USF-IMARS/seadas_spa/blob/port/wrapper/oc/installation.xml)
-
-
 FIL1KM=$1
-file_type=`file ${FIL1KM} | grep 'Hierarchical Data Format (version 4) data'`
 
+set -e
+# set -o verbose  # for debugging help
+
+file_type=`file ${FIL1KM}`
 echo ft: $file_type
 
-if [ -n "$file_type" ]
+# if hdf4
+if [ `echo ${file_type} | grep 'Hierarchical Data Format (version 4) data'` ]
 then
 	DAY_FLAG=`${MODIS_DB_HOME}/run/bin3/linux/ncdump_hdf -h ${FIL1KM} | grep "Day"`
-else
+else  # assume it's hdf5
 	DAY_FLAG=`${MODIS_DB_HOME}/run/bin3/linux/h5dump -a /Data_Products/VIIRS-MOD-GEO-TC/VIIRS-MOD-GEO-TC_Gran_0/N_Day_Night_Flag ${FIL1KM} | grep  \"Day\"`
 fi
 
